66dbf83b703376bd845b8fd5df225ace89b69032
samuelswank <samswank@gmail.com>
2026-05-09 13:36:27 -0600

    Automatic shutdown after an hour of the window being closed / inactivity

--- SearchTracker/urls.py
+++ SearchTracker/urls.py
@@ -5,5 +5,7 @@ app_name = "SearchTracker"
 
 urlpatterns = [
     path('', views.HomeTemplateView.as_view(), name="home"),
-    path("close/", views.close, name="close"),
+    path("documents/", views.documents, name="documents"),
+    path("heartbeat/", views.heartbeat, name="heartbeat"),
+    path("close/", views.close_app, name="close"),
 ]
--- SearchTracker/views.py
+++ SearchTracker/views.py
@@ -1,9 +1,13 @@
+import time
+import threading
 from django.views.generic import TemplateView
 from .models import Document, TemplateSnippet
 from django.shortcuts import render
+from django.http import HttpResponse
 import os
 import signal
 from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
@@ -11,6 +15,39 @@ from django.views.decorators.csrf import csrf_exempt
 
 APP_NAME = "SearchTracker"
 ADMIN_PREFIX = f"/admin/{APP_NAME}/"
+
+# variables
+_last_heartbeat = time.time()
+_heartbeat_lock = threading.Lock()
+_watchdog_thread_started = False
+
+# non view utilities
+
+
+def close():
+    # Get the process group ID of the current process
+    pgid = os.getpgid(os.getpid())
+    # Send SIGKILL to every process in this group
+    os.killpg(pgid, signal.SIGKILL)
+
+
+def _watchdog():
+    global _last_heartbeat
+    while True:
+        if time.time() - _last_heartbeat > 60 * 60 * 1000:
+            close()
+            break
+
+        time.sleep(5)
+
+
+def _start_watchdog():
+    global _watchdog_thread_started
+    if not _watchdog_thread_started:
+        _watchdog_thread_started = True
+        thread = threading.Thread(target=_watchdog, daemon=True)
+        thread.start()
+
 
 # class based views
 
@@ -35,8 +72,14 @@ def documents(request):
 
 
 @csrf_exempt
-def close(request):
-    # Get the process group ID of the current process
-    pgid = os.getpgid(os.getpid())
-    # Send SIGKILL to every process in this group
-    os.killpg(pgid, signal.SIGKILL)
+def heartbeat(request):
+    global _last_heartbeat
+    with _heartbeat_lock:
+        _last_heartbeat = time.time()
+    _start_watchdog()
+    return HttpResponse("Shutting down...")
+
+
+@csrf_exempt
+def close_app(request):
+    close()
